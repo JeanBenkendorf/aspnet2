477d4f794cb3d015fd78ae1cdf0918c4423f88e5
Jean Benkendorf <jean.benkendorf@outlook.com>
2018-10-04 19:50:40 -0300

    Criação Classe Usurio

---
+++ WebApplication3/Data/Usuarios.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3.Data
+{
+    public class Usuarios
+    {
+        public int UsuarioId { get; set; }
+        public int Nome { get; set; }
+        public int Senha { get; set; }
+    }
+}
