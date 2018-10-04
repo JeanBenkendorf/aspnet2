18fc43efae263acdaf56911447b1a5bb8b7c58cb
Jean Benkendorf <jean.benkendorf@outlook.com>
2018-10-04 20:30:31 -0300

    Criação de Login e senha

--- WebApplication3/Data/Usuario.cs
+++ WebApplication3/Data/Usuario.cs
@@ -5,10 +5,10 @@ using System.Threading.Tasks;
 
 namespace WebApplication3.Data
 {
-    public class Usuarios
+    public class Usuario
     {
         public int UsuarioId { get; set; }
-        public int Nome { get; set; }
-        public int Senha { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
     }
 }
--- WebApplication3/Pages/Usuarios/Cadastrar.cshtml.cs
+++ WebApplication3/Pages/Usuarios/Cadastrar.cshtml.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApplication3.Data;
 
 namespace WebApplication3.Pages.Usuarios
 {
     public class CadastrarModel : PageModel
     {
-        public void OnGet()
-        {
 
+        [BindProperty]
+       public Usuario usuarios { get; set; }
+       public void Onget(string nome, string senha)
+        {
+            if (usuarios == null)
+            {
+                usuarios = new Usuario();
+            }
+            usuarios.Nome = nome;
+            usuarios.Senha = senha;
         }
     }
 }
